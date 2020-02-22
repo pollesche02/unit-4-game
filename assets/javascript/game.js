4dd718590362a84b1bb5e9291d087e45a51293b2
Danielle Pollesche <dani@kcsburningaromas.com>
2020-02-22 16:12:09 -0700

    changed my javascript to be updated with new numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,41 @@
 
+var targetNumber;
+var numberOptions;
+var counterTotal = 0;
+
+$("#counter-total").text(counterTotal);
+$("#counter-total").empty();
+var wins = 0;
+$("#wins").text(wins);
+var losses = 0;
+$("#losses").text(losses);
+
+var crystalOne = $("#crystal-1");
+var crystalTwo = $("#crystal-2");
+var crystalThree = $("#crystal-3");
+var crystalFour = $("#crystal-4");
+var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var ranNums = [];
+targetNumber = Math.floor(Math.random() * (120-19)) +19;
+
+$(document).ready(function() {
+  var topRanPixels = Math.floor(Math.random() * 300);
+
+  $("#current-random-num").text(counterTotal);
+
+  function restart() {
+    counterTotal = 0;
+    $("#counter-total").text(counterTotal);
+    $("#counter-total").empty();
+    $("#current-random-num").empty()
+    targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+    $("#current-random-num").text(targetNumber);
+    ranNums = [];
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    randomCrystalValues();
+  }
+
+  function randomCrystalValues() {
+    
+  }
+})
