7c842def25dc0c2dfa42c82e7cd3164b007fe7d7
Danielle Pollesche <dani@kcsburningaromas.com>
2020-02-24 22:04:11 -0700

    changed js code updated it to start pulling random number per 'crystal'

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,57 @@
+var random = 0
+var score = 0
+var wins = 0
+var losses = 0
+var crystals = 0
 
-var targetNumber;
-var numberOptions;
-var counterTotal = 0;
+$(document).ready(function () {
+    startGame()
+})
 
-$("#counter-total").text(counterTotal);
-$("#counter-total").empty();
-var wins = 0;
-$("#wins").text(wins);
-var losses = 0;
-$("#losses").text(losses);
-
-var crystalOne = $("#crystal-1");
-var crystalTwo = $("#crystal-2");
-var crystalThree = $("#crystal-3");
-var crystalFour = $("#crystal-4");
-var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-var ranNums = [];
-targetNumber = Math.floor(Math.random() * (120-19)) +19;
-
-$(document).ready(function() {
-  var topRanPixels = Math.floor(Math.random() * 300);
-
-  $("#current-random-num").text(counterTotal);
-
-  function restart() {
-    counterTotal = 0;
-    $("#counter-total").text(counterTotal);
-    $("#counter-total").empty();
-    $("#current-random-num").empty()
-    targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
-    $("#current-random-num").text(targetNumber);
-    ranNums = [];
-    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    randomCrystalValues();
-  }
-
-  function randomCrystalValues() {
-    (i = numbs.length), (j = 0);
-    nums.splice(j, 1);
-  }
+function startGame() {
+    var randnum1 = Math.floor(Math.random() * 19) + 19;
+    $("#blueflower").attr("value", randnum1);
+    
+    console.log(randnum1);
+    var randnum2 = Math.floor(Math.random() * 19) + 19;
+    $("#diamond2").attr("value", randnum2);
+    console.log(randnum2);
+    var randnum3 = Math.floor(Math.random() * 19) + 19;
+    $("#greenheart").attr("value", randnum3);
+    console.log(randnum3);
+    var randnum4 = Math.floor(Math.random() * 19) + 19;
+    $("#redstar").attr("value", randnum4);
+    console.log(randnum4);
+    random = Math.floor(Math.random() * 100) + 19;
+    $("#random").text("Match Number: " + random);
+    console.log(randnum1, random);
+    var targetSum = Math.floor(Math.random() * 100) + 19;
 }
-randomCrystalValues();
 
-$(".crystal-image").on("click", function() {
-  $(crystalOne).attr("data-crystalvalue", ranNums[0]);
-  $(crystalTwo).attr("data-crystalvalue", ranNums[1]);
-  $(crystalThree).attr("data-crystalvalue", ranNums[2]);
-  $(crystalFour).attr("data-crystalvalue"), ranNums[3]);
+$("button").on("click", function (event) {
+    event.preventDefault();
+    var gemValue = $(this).attr("value");
+    score += parseInt(gemValue);
+    console.log(score);
+    $("#score").text("Your Total Score: "  + score);
+    if (score === random) {
+        wins++;
+        alert("You Win!");
+        $("#wins").text("Wins: "  + wins);
+        $("#score").text("Your Total Score: "  + score);
+        reset();
+    }
+    else if (score > random) {
+        losses++;
+        alert("You Lose! :(")
+        $("#losses").text("Losses: " + losses);
+        reset();
+    }
+})
 
-  var crystalValue = $(this).attr("data-crystalvalue");
-  crystalValue = parseInt(crystalValue);
-
-  // I have to add the value of every crystal that is clicked and make 
-  //sure they are added together everytime a crystal is clicked
-})counterTotal += crystalValue;
-$("#counter-total").text(counterTotal);
-
- if (counterTotal == targetNumber) {
-   alert("You Win!");
-   $("#wins").text(wins);
-   restart();
- } else if (counterTotal >+ targetNumber) {
-   alert("You Lose!");
-   losses++;
-   $("#losses").text(losses);
-   restart();
-  }
-  }); 
-});
+function reset() {
+    alert("Let's Collect More Crystals!");
+    score = 0;
+    $("#score").text("Your Total Score: " + score);
+    startGame();
+}
